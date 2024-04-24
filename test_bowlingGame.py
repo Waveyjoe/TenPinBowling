5dbd735e233dca46908e5b5b72f8d7976bf837a0
Waveyjoe <77323471+Waveyjoe@users.noreply.github.com>
2024-04-24 19:35:43 +1200

    Test Added: Consecutive Strikes
    Test for consecutive strikes followed by open frames.

--- test_bowlingGame.py
+++ test_bowlingGame.py
@@ -14,31 +14,52 @@ class TestBowlingGame(unittest.TestCase):
     def testGutterGame(self):
         for i in range(0, 20):
             self.game.roll(0)
-        assert self.game.score()==0
+        self.assertEqual(self.game.score(), 0)
+
+
     def testAllOnes(self):
         self.rollMany(1, 20)
-        assert self.game.score()==20
+        self.assertEqual(self.game.score(), 20)
+
     def testOneSpare(self):
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
         self.rollMany(0,17)
-        assert self.game.score()==16
+        self.assertEqual(self.game.score(), 16)
+
+
     def testOneStrike(self):
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
         self.rollMany(0,16)
-        assert  self.game.score()==24
+        self.assertEqual(self.game.score(), 24)
+
+    def testConsecutiveStrikes(self):
+        self.game.roll(10)
+        self.game.roll(10)
+        self.game.roll(4)
+        self.game.roll(3)
+        self.rollMany(0,16)
+        self.assertEqual(self.game.score(), 48)
+
+
+        
+
     def testPerfectGame(self):
         self.rollMany(10,12)
-        assert self.game.score()==300
+        self.assertEqual(self.game.score(), 300)
+
     def testOneSpare(self):
         self.rollMany(5,21)
-        assert self.game.score()==150
+        self.assertEqual(self.game.score(), 150)
+    
+
     def rollMany(self, pins,rolls):
         for i in range(rolls):
             self.game.roll(pins)
+
 
 
 if __name__ == '__main__':
